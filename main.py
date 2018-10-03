6f68802ef8fa4e7dd4e5ea67df94204a811c88a5
mr-ego <38979459+mr-ego@users.noreply.github.com>
2018-10-03 21:40:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
 ​
 def intent_received(hermes, intent_message):
-    print('Intent {}'.format(intent_message.intent))
-​
-    hermes.publish_end_session(intent_message.session_id, None)
-​
+    result_sentence = "hello in this lonely beach"
+    hermes.publish_end_session(intent_message.session_id, result_sentence)
+
 with Hermes('localhost:1883') as h:
     h.subscribe_intents(intent_received).start()
